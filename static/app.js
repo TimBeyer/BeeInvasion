1745aaf6a37366740e630bc7da0ea48a5b87d304
Tim Beyer <tim.beyer@deliveryhero.com>
2013-03-09 14:10:51 +0100

    Modularize code

--- static/app.js
+++ static/app.js
@@ -1,11 +1,41 @@
+window.beeInvasion = (function (beeInvasion) {
+
+    beeInvasion.Game = function (options) {
+        var canvas = options.canvas;
+        var ctx = canvas.getContext("2d");
+        // Assume array [x,y]
+        var canvasSize = options.canvasSize;
+        canvas.width = canvasSize[0];
+        canvas.height = canvasSize[1];
+
+
+        this.clearScreen = function () {
+            ctx.fillStyle = "rgb(0,0,0)";
+            ctx.fillRect(0,0,canvasSize[0], canvasSize[1]);
+        };
+
+        this.run = function () {
+            this.clearScreen();
+            console.log("Loop");
+            window.requestAnimationFrame(_.bind(this.run, this));
+        };
+    };
+
+    return beeInvasion;
+}(window.beeInvasion || {}));
+
 (function () {
     $(function () {
+        var body = $('body');
+        var bodyWidth = body.width();
+        var bodyHeight = body.height();
         
-        var canvas = $('#gameCanvas')[0];
-        var ctx = canvas.getContext("2d");
-        var canvasWidth = canvas.width;
-        var canvasHeight = canvas.height;
-        ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+        var game = new beeInvasion.Game({
+            canvas: $('#gameCanvas')[0],
+            canvasSize: [bodyWidth, bodyHeight]
+        });
+
+        game.run();
 
     });
 }());
